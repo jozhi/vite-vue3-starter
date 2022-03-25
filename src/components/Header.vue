1eb341837e80507788376489b010a2f50fdfd0f7
jiaosz <11194720@qq.com>
2022-03-25 16:31:43 +0800

    init page

---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="header">
+    <div class="title" @click="router.push('/')">
+      Vite2.x + Vue3.x + TypeScript Starter
+    </div>
+    <!-- <div class="go-github" @click="goGitHub">
+      <i class="icon el-icon-s-promotion"></i> GitHub
+    </div> -->
+
+    <Nav />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import Nav from '@/components/Nav.vue'
+
+// eslint-disable-next-line no-unused-vars
+// const router = useRouter()
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+    Nav
+  },
+  setup() {
+    const router = useRouter()
+
+    return {
+      router
+    }
+  }
+})
+
+// // eslint-disable-next-line no-unused-vars
+// const goGitHub = () => {
+//   window.open('https://github.com/XPoet/vite-vue3-starter')
+// }
+</script>
+
+<style scoped lang="stylus">
+
+.header {
+  width 100%
+  height 100%
+  background #fff
+  display flex
+  justify-content space-between
+  align-items center
+  padding 0 20px
+  box-sizing border-box
+  font-weight bold
+
+  .title {
+    font-size 20px
+    cursor pointer
+  }
+
+  // .go-github {
+  //   cursor pointer
+  //   font-size 16px
+
+  //   .icon {
+  //     font-size 20px
+  //   }
+  // }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="home-container page-container">
+    <!-- <img class="vue-element-plus-logo" alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <div class="page-title">Vite2.x + Vue3.x + TypeScript + Element Plus</div> -->
+    <div id="myEcharts"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onUnmounted } from 'vue'
+
+import * as echarts from 'echarts'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    /// 声明定义一下echart
+    const echart = echarts
+
+    // 基础配置一下Echarts
+    function initChart() {
+      const chart = echart.init(document.getElementById('myEcharts')!, 'dark')
+      // 把配置和数据放这里
+      chart.setOption({
+        xAxis: {
+          type: 'category',
+          data: [
+            '一月',
+            '二月',
+            '三月',
+            '四月',
+            '五月',
+            '六月',
+            '七月',
+            '八月',
+            '九月',
+            '十月',
+            '十一月',
+            '十二月'
+          ]
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320, 801, 102, 230, 4321, 4129],
+            type: 'line',
+            smooth: true
+          }
+        ]
+      })
+
+      window.onresize = () => {
+        // 自适应大小
+        chart.resize()
+      }
+    }
+
+    onMounted(() => {
+      initChart()
+    })
+    onUnmounted(() => {
+      // eslint-disable-next-line no-unused-expressions
+      echart.dispose
+    })
+    return { initChart }
+  }
+})
+</script>
+
+<style scoped lang="stylus">
+.home-container {
+  // .vue-element-plus-logo {
+  //   width 50%
+  // }
+
+  #myEcharts{
+    width 100%
+    height 500px
+  }
+}
+</style>
